e69277f56bf3e284196f13dd225c3f759283c1f8
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2024-12-15 23:37:05 +0100

    Dodanie obsługi funkcji dodawania, aktualizowania i usuwania danych z zoo.json za pomocą AnimalController.js

--- controllers/AnimalController.js
+++ controllers/AnimalController.js
@@ -32,3 +32,28 @@ app.get('/species', (req, res) => {
     const animals = AnimalsService.getAnimalsBySpecies(species);
     res.json(animals);
 });
+
+app.post('/addanimal', (req, res) => {
+    const newAnimal = AnimalsService.addAnimal(req.body);
+    res.status(201).json(newAnimal);
+});
+
+app.put('/updateanimal/:id', (req, res) => {
+    const updatedAnimal = AnimalsService.updateAnimal(Number(req.params.id), req.body);
+    if (updatedAnimal) {
+        res.json(updatedAnimal);
+    } else {
+        res.status(404).json({ message: 'Animal not found' });
+    }
+});
+
+app.delete('/:id', (req, res) => {
+    const isDeleted = AnimalsService.deleteAnimal(Number(req.params.id));
+    if (isDeleted) {
+        res.json({ message: 'Animal deleted successfully' });
+    } else {
+        res.status(404).json({ message: 'Animal not found' });
+    }
+});
+
+export default app;
